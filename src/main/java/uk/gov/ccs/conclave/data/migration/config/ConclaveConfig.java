8f02431238f256a0167fee20d71cb972aa1ec2e1
janice.alvares <janice.alvares@crowncommercial.gov.uk>
2021-07-27 13:13:44 +0100

    Added cii integration

--- src/main/java/uk/gov/ccs/conclave/data/migration/config/ConclaveConfig.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/config/ConclaveConfig.java
@@ -1,4 +1,4 @@
-package uk.gov.ccs.conclave.data.migration.integration;
+package uk.gov.ccs.conclave.data.migration.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -7,13 +7,13 @@ import uk.gov.ccs.swagger.sso.ApiClient;
 import uk.gov.ccs.swagger.sso.api.UserApi;
 
 @Configuration
-public class ConclaveIntegrationConfig {
+public class ConclaveConfig {
 
     private final String apiKey;
 
     private final String origin;
 
-    public ConclaveIntegrationConfig(@Value("${conclave.api.key}") String apiKey, @Value("${conclave.host}") String origin) {
+    public ConclaveConfig(@Value("${conclave.api.key}") String apiKey, @Value("${conclave.host}") String origin) {
         this.apiKey = apiKey;
         this.origin = origin;
     }
@@ -23,7 +23,7 @@ public class ConclaveIntegrationConfig {
         return new UserApi(apiClient());
     }
 
-    @Bean
+    @Bean("conclaveClient")
     public ApiClient apiClient() {
         return new ApiClient()
                 .addDefaultHeader("x-api-key", apiKey)
