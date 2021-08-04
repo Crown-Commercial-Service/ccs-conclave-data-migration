84229f46e052b63201157bc4430ed16093599231
jalvares-crown <70591203+jalvares-crown@users.noreply.github.com>
2021-08-04 15:05:36 +0100

    Configs (#74)
    * Vault endpoint update

--- src/main/java/uk/gov/ccs/conclave/data/migration/MigrationApplication.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/MigrationApplication.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.core.VaultOperations;
 import uk.gov.ccs.conclave.data.migration.config.MigrationProperties;
 import uk.gov.ccs.conclave.data.migration.config.VaultMigrationConfiguration;
 
@@ -14,7 +14,7 @@ import uk.gov.ccs.conclave.data.migration.config.VaultMigrationConfiguration;
 public class MigrationApplication implements CommandLineRunner {
 
     @Autowired
-    VaultTemplate template;
+    VaultOperations operations;
 
 
     public static void main(String[] args) {
@@ -28,9 +28,9 @@ public class MigrationApplication implements CommandLineRunner {
 
         LOGGER.info("----------------------------------------");
         LOGGER.info("Configuration properties");
-        LOGGER.info("		migration properties is {}", VaultMigrationConfiguration.readSecrets(template).getConclaveOrigin());
+        LOGGER.info("		migration properties is {}", VaultMigrationConfiguration.readSecrets(operations).getConclaveOrigin());
         //  LOGGER.info("		migration properties is {}", vaultConfiguration.readSecrets().getConclaveOrigin());
-         LOGGER.info("		dm.conclaveHost using template is  {}", template.read("cf/5718307e-5904-4fcc-8660-f6d603ba81dd/secret/migration", MigrationProperties.class).getData().getConclaveOrigin());
+         LOGGER.info("		dm.conclaveHost using template is  {}", operations.read("cf/5718307e-5904-4fcc-8660-f6d603ba81dd/secret/migration", MigrationProperties.class).getData().getConclaveOrigin());
         LOGGER.info("----------------------------------------");
     }
 
--- src/main/java/uk/gov/ccs/conclave/data/migration/config/VaultMigrationConfiguration.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/config/VaultMigrationConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.vault.authentication.ClientAuthentication;
 import org.springframework.vault.authentication.TokenAuthentication;
 import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.config.AbstractVaultConfiguration;
-import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.core.VaultOperations;
 import org.springframework.vault.support.VaultResponseSupport;
 
 import java.net.URI;
@@ -33,8 +33,8 @@ public class VaultMigrationConfiguration extends AbstractVaultConfiguration {
         return VaultEndpoint.create(vaultUri.getHost(), vaultUri.getPort());
     }
 
-    public static MigrationProperties readSecrets(VaultTemplate template) {
-        VaultResponseSupport<MigrationProperties> response = template.read(cfCredentials.getString("backends_shared", "space"), MigrationProperties.class);
+    public static MigrationProperties readSecrets(VaultOperations operations) {
+        VaultResponseSupport<MigrationProperties> response = operations.read(cfCredentials.getString("backends_shared", "space"), MigrationProperties.class);
         return Objects.requireNonNull(response).getData();
     }
 
