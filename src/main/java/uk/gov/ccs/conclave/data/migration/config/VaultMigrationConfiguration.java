b462893a7126a8e1626f3ffdd7a5436ce189340a
janice.alvares <janice.alvares@crowncommercial.gov.uk>
2021-08-04 12:24:54 +0100

    Usinf cfenv lib

--- src/main/java/uk/gov/ccs/conclave/data/migration/config/VaultMigrationConfiguration.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/config/VaultMigrationConfiguration.java
@@ -1,11 +1,10 @@
 package uk.gov.ccs.conclave.data.migration.config;
 
-import io.pivotal.spring.cloud.vault.service.common.VaultServiceInfo;
+import io.pivotal.cfenv.core.CfCredentials;
+import io.pivotal.cfenv.core.CfEnv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.Cloud;
-import org.springframework.cloud.CloudFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.vault.authentication.ClientAuthentication;
 import org.springframework.vault.authentication.TokenAuthentication;
@@ -13,6 +12,9 @@ import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.config.AbstractVaultConfiguration;
 import org.springframework.vault.core.VaultOperations;
 import org.springframework.vault.support.VaultResponseSupport;
+
+import java.net.URI;
+import java.util.Objects;
 
 @Configuration
 public class VaultMigrationConfiguration extends AbstractVaultConfiguration {
@@ -22,28 +24,28 @@ public class VaultMigrationConfiguration extends AbstractVaultConfiguration {
     @Autowired
     private VaultOperations operations;
 
-    VaultServiceInfo vaultService = getVaultServiceInfo();
+    CfCredentials cfCredentials = getVaultServiceInfo();
 
     @Override
     public ClientAuthentication clientAuthentication() {
-        return new TokenAuthentication(String.valueOf(vaultService.getToken()));
+        return new TokenAuthentication(cfCredentials.getString("auth", "token"));
     }
 
     @Override
     public VaultEndpoint vaultEndpoint() {
-        return VaultEndpoint.create(vaultService.getHost(), vaultService.getPort());
+        return VaultEndpoint.from(URI.create(cfCredentials.getString("address")));
     }
 
     public MigrationProperties readSecrets() {
-        VaultResponseSupport<MigrationProperties> response = operations.read(vaultService.getSharedBackends().get("space"), MigrationProperties.class);
-        return response.getData();
+        VaultResponseSupport<MigrationProperties> response = operations.read(cfCredentials.getString("backends_shared", "space"), MigrationProperties.class);
+        return Objects.requireNonNull(response).getData();
     }
 
-    private static VaultServiceInfo getVaultServiceInfo() {
-        CloudFactory cloudFactory = new CloudFactory();
-        Cloud cloud = cloudFactory.getCloud();
-        VaultServiceInfo vaultServiceInfo = (io.pivotal.spring.cloud.vault.service.common.VaultServiceInfo) cloud.getServiceInfo("vault-service-data-migration-sandbox");
-        LOGGER.info("CLOUD INFO is : " + vaultServiceInfo.toString());
-        return vaultServiceInfo;
+    private static CfCredentials getVaultServiceInfo() {
+        CfEnv cfEnv = new CfEnv();
+        CfCredentials credentials = cfEnv.findCredentialsByName("vault-service-data-migration-sandbox");
+        LOGGER.info("reading credentials :: host" + credentials.getHost());
+        LOGGER.info("reading map :: host" + credentials.getMap().toString());
+        return credentials;
     }
 }
