9e63fef2f9e9e991a5b274193ad997e407355af1
jalvares-crown <70591203+jalvares-crown@users.noreply.github.com>
2021-08-04 16:20:02 +0100

    Debug (#76)
    * Extra logging

--- src/main/java/uk/gov/ccs/conclave/data/migration/config/VaultMigrationConfiguration.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/config/VaultMigrationConfiguration.java
@@ -5,6 +5,7 @@ import io.pivotal.cfenv.core.CfEnv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.vault.authentication.ClientAuthentication;
 import org.springframework.vault.authentication.TokenAuthentication;
 import org.springframework.vault.client.VaultEndpoint;
@@ -12,11 +13,14 @@ import org.springframework.vault.config.AbstractVaultConfiguration;
 import org.springframework.vault.core.VaultOperations;
 import org.springframework.vault.support.VaultResponseSupport;
 
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Objects;
 
 @Configuration
+@Profile("cloud")
 public class VaultMigrationConfiguration extends AbstractVaultConfiguration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(VaultMigrationConfiguration.class);
@@ -33,7 +37,7 @@ public class VaultMigrationConfiguration extends AbstractVaultConfiguration {
         VaultEndpoint endpoint = null;
         try {
             String address = cfCredentials.getString("address");
-            LOGGER.debug("Address from environment is " + address);
+            LOGGER.info("Address from environment is " + address);
 
             endpoint = VaultEndpoint.from(new URI(address));
         } catch (URISyntaxException e) {
@@ -43,7 +47,9 @@ public class VaultMigrationConfiguration extends AbstractVaultConfiguration {
     }
 
     public static MigrationProperties readSecrets(VaultOperations operations) {
-        VaultResponseSupport<MigrationProperties> response = operations.read(cfCredentials.getString("backends_shared", "space"), MigrationProperties.class);
+        String backendPath = cfCredentials.getString("backends_shared", "space");
+        LOGGER.info("BACKENd path " + backendPath);
+        VaultResponseSupport<MigrationProperties> response = operations.read(backendPath.concat("migration"), MigrationProperties.class);
         return Objects.requireNonNull(response).getData();
     }
 
