5e6b9a6df1f7ddfeecf40b9b8b6458bc5de37e54
jalvares-crown <70591203+jalvares-crown@users.noreply.github.com>
2021-08-10 11:16:41 +0100

    Updated PropertiesConfig (#88)

--- src/main/java/uk/gov/ccs/conclave/data/migration/config/PropertiesConfig.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/config/PropertiesConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.vault.core.VaultOperations;
 
-import javax.annotation.PostConstruct;
 import java.util.Objects;
 
 import static uk.gov.ccs.conclave.data.migration.config.VaultMigrationConfig.getBackendPath;
@@ -14,22 +13,14 @@ public class PropertiesConfig {
 
     protected final VaultOperations operations;
 
-    @lombok.Setter
-    private MigrationProperties migrationProperties;
-
     @Bean
-    public MigrationProperties getMigrationProperties() {
-        return migrationProperties;
+    public MigrationProperties migrationProperties() {
+        var response = operations.read(getBackendPath(), MigrationProperties.class);
+        return Objects.requireNonNull(response).getData();
     }
 
     public PropertiesConfig(VaultOperations operations) {
         this.operations = operations;
-    }
-
-    @PostConstruct
-    public void readSecrets() {
-        var response = operations.read(getBackendPath(), MigrationProperties.class);
-        setMigrationProperties(Objects.requireNonNull(response).getData());
     }
 
 }
