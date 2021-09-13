778d067a9d93fd3b0446c1f0971f5d9d87f00e6f
janice.alvares <janice.alvares@crowncommercial.gov.uk>
2021-09-13 16:38:04 +0100

    Minor refactor

--- src/main/java/uk/gov/ccs/conclave/data/migration/service/SummaryService.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/service/SummaryService.java
@@ -41,33 +41,35 @@ public class SummaryService {
     }
 
     public Summary buildSummaryWithStatus(Organisation org, int statusCode) {
+        Summary summary;
         if (statusCode == 200) {
-            return buildSummary(org, Status._200_OK);
+            summary = buildSummary(org, Status._200_OK);
         } else if (statusCode == 201) {
-            return buildSummary(org, Status._201_CREATED);
+            summary = buildSummary(org, Status._201_CREATED);
         } else if (statusCode == 400) {
-            return buildSummary(org, Status._400_BAD_REQUEST);
+            summary = buildSummary(org, Status._400_BAD_REQUEST);
         } else if (statusCode == 401) {
-            return buildSummary(org, Status._401_UNATHORIZED);
+            summary = buildSummary(org, Status._401_UNATHORIZED);
         } else if (statusCode == 403) {
-            return buildSummary(org, Status._403_FORBIDDEN);
+            summary = buildSummary(org, Status._403_FORBIDDEN);
         } else if (statusCode == 404) {
-            return buildSummary(org, Status._404_NOT_FOUND);
+            summary = buildSummary(org, Status._404_NOT_FOUND);
         } else if (statusCode == 409) {
-            return buildSummary(org, Status._409_DUPLICATE_RESOURCE);
+            summary = buildSummary(org, Status._409_DUPLICATE_RESOURCE);
         } else if (statusCode == 429) {
-            return buildSummary(org, Status._429_TOO_MANY_REQUESTS);
+            summary = buildSummary(org, Status._429_TOO_MANY_REQUESTS);
         } else if (statusCode == 500) {
-            return buildSummary(org, Status._500_INTERNAL_SERVER_ERROR);
+            summary = buildSummary(org, Status._500_INTERNAL_SERVER_ERROR);
         } else if (statusCode == 503) {
-            return buildSummary(org, Status._503_SERVICE_UNAVAILABLE_LIMIT_EXCEEDED);
+            summary = buildSummary(org, Status._503_SERVICE_UNAVAILABLE_LIMIT_EXCEEDED);
         } else if (statusCode == 504) {
-            return buildSummary(org, Status._504_GATEWAY_TIMEOUT);
+            summary = buildSummary(org, Status._504_GATEWAY_TIMEOUT);
         } else if (statusCode == 505) {
-            return buildSummary(org, Status._505_HTTP_VERSION_NOT_SUPPORTED);
+            summary = buildSummary(org, Status._505_HTTP_VERSION_NOT_SUPPORTED);
         } else {
-            return buildSummary(org, Status._505_HTTP_VERSION_NOT_SUPPORTED);
+            summary = buildSummary(org, Status._505_HTTP_VERSION_NOT_SUPPORTED);
         }
+        return summary;
     }
 
     private Summary buildSummary(Organisation org, Status status) {
