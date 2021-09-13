b689febe561f0b3411af1b33d174c9160cac4ff1
janice.alvares <janice.alvares@crowncommercial.gov.uk>
2021-09-13 16:46:01 +0100

    Switch cases

--- src/main/java/uk/gov/ccs/conclave/data/migration/service/SummaryService.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/service/SummaryService.java
@@ -42,32 +42,43 @@ public class SummaryService {
 
     public Summary buildSummaryWithStatus(Organisation org, int statusCode) {
         Summary summary;
-        if (statusCode == 200) {
-            summary = buildSummary(org, Status._200_OK);
-        } else if (statusCode == 201) {
-            summary = buildSummary(org, Status._201_CREATED);
-        } else if (statusCode == 400) {
-            summary = buildSummary(org, Status._400_BAD_REQUEST);
-        } else if (statusCode == 401) {
-            summary = buildSummary(org, Status._401_UNATHORIZED);
-        } else if (statusCode == 403) {
-            summary = buildSummary(org, Status._403_FORBIDDEN);
-        } else if (statusCode == 404) {
-            summary = buildSummary(org, Status._404_NOT_FOUND);
-        } else if (statusCode == 409) {
-            summary = buildSummary(org, Status._409_DUPLICATE_RESOURCE);
-        } else if (statusCode == 429) {
-            summary = buildSummary(org, Status._429_TOO_MANY_REQUESTS);
-        } else if (statusCode == 500) {
-            summary = buildSummary(org, Status._500_INTERNAL_SERVER_ERROR);
-        } else if (statusCode == 503) {
-            summary = buildSummary(org, Status._503_SERVICE_UNAVAILABLE_LIMIT_EXCEEDED);
-        } else if (statusCode == 504) {
-            summary = buildSummary(org, Status._504_GATEWAY_TIMEOUT);
-        } else if (statusCode == 505) {
-            summary = buildSummary(org, Status._505_HTTP_VERSION_NOT_SUPPORTED);
-        } else {
-            summary = buildSummary(org, Status._505_HTTP_VERSION_NOT_SUPPORTED);
+        switch (statusCode) {
+            case 200:
+                summary = buildSummary(org, Status._200_OK);
+                break;
+            case 201:
+                summary = buildSummary(org, Status._201_CREATED);
+                break;
+            case 400:
+                summary = buildSummary(org, Status._400_BAD_REQUEST);
+                break;
+            case 401:
+                summary = buildSummary(org, Status._401_UNATHORIZED);
+                break;
+            case 403:
+                summary = buildSummary(org, Status._403_FORBIDDEN);
+                break;
+            case 404:
+                summary = buildSummary(org, Status._404_NOT_FOUND);
+                break;
+            case 409:
+                summary = buildSummary(org, Status._409_DUPLICATE_RESOURCE);
+                break;
+            case 429:
+                summary = buildSummary(org, Status._429_TOO_MANY_REQUESTS);
+                break;
+            case 500:
+                summary = buildSummary(org, Status._500_INTERNAL_SERVER_ERROR);
+                break;
+            case 503:
+                summary = buildSummary(org, Status._503_SERVICE_UNAVAILABLE_LIMIT_EXCEEDED);
+                break;
+            case 504:
+                summary = buildSummary(org, Status._504_GATEWAY_TIMEOUT);
+                break;
+            default:
+                summary = buildSummary(org, Status._505_HTTP_VERSION_NOT_SUPPORTED);
+                break;
         }
         return summary;
     }
