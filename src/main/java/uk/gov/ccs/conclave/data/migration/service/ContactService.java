f4cb3e654e026504d53c1a0f5f65a46608d3ffa0
janice.alvares <janice.alvares@crowncommercial.gov.uk>
2022-02-03 11:10:41 +0000

    Updated contact service

--- src/main/java/uk/gov/ccs/conclave/data/migration/service/ContactService.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/service/ContactService.java
@@ -26,37 +26,23 @@ public class ContactService {
     private final ErrorService errorService;
 
     void migrateUserContact(User user, String userId, Org organisation) {
-        if (isUserContactDetailPresent(user)) {
+        ContactPoint userContactPoint = new ContactPoint();
+        userContactPoint.setEmail(user.getContactEmail());
+        userContactPoint.setFaxNumber(user.getContactFax());
+        userContactPoint.setTelephone((user.getContactPhone() != null) ? user.getContactPhone() : user.getContactMobile());
+        userContactPoint.setUri(user.getContactSocial());
+        if (isContactDetailPresent(userContactPoint)) {
             try {
-                conclaveClient.createUserContact(userId, buildUserContactRequestInfo(user));
+                conclaveClient.createUserContact(userId, buildContactRequestInfo(userContactPoint));
             } catch (uk.gov.ccs.swagger.sso.ApiException e) {
                 errorService.saveUserDetailWithStatusCode(user, SSO_USER_CONTACT_ERROR_MESSAGE + e.getMessage(), e.getCode(), organisation);
             }
         }
     }
 
-    private ContactRequestInfo buildUserContactRequestInfo(User user) {
-        ContactRequestInfo contactRequestInfo = new ContactRequestInfo();
-        List<ContactRequestDetail> userContacts = new ArrayList<>();
-        userContacts.add(buildContactRequestDetail("EMAIL", user.getContactEmail()));
-        userContacts.add(buildContactRequestDetail("PHONE", (user.getContactPhone() != null) ? user.getContactPhone() : user.getContactMobile()));
-        userContacts.add(buildContactRequestDetail("FAX", user.getContactFax()));
-        userContacts.add(buildContactRequestDetail("WEB_ADDRESS", user.getContactSocial()));
-        contactRequestInfo.setContacts(userContacts);
-        return contactRequestInfo;
-    }
-
-    private boolean isUserContactDetailPresent(User user) {
-        return (!user.getContactEmail().isEmpty()
-                || !user.getContactMobile().isEmpty()
-                || !user.getContactFax().isEmpty()
-                || !user.getContactSocial().isEmpty()
-                || !user.getContactPhone().isEmpty());
-    }
-
     void migrateOrgContact(Organisation org, OrgMigration ciiResponse, String organisationId) {
         ContactPoint contactPoint = ciiResponse.getContactPoint();
-        if (isOrgContactDetailPresent(contactPoint)) {
+        if (isContactDetailPresent(contactPoint)) {
             try {
                 conclaveClient.createOrganisationContact(organisationId, buildContactRequestInfo(contactPoint));
             } catch (uk.gov.ccs.swagger.sso.ApiException e) {
@@ -65,7 +51,7 @@ public class ContactService {
         }
     }
 
-    private boolean isOrgContactDetailPresent(ContactPoint contactPoint) {
+    private boolean isContactDetailPresent(ContactPoint contactPoint) {
         return (!contactPoint.getName().isEmpty()
                 || !contactPoint.getEmail().isEmpty()
                 || !contactPoint.getTelephone().isEmpty()
