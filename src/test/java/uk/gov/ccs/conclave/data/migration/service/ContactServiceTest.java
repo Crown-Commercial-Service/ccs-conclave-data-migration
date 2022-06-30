bd13741b9a7d1e106b1f5bdeaa108a17aa232a79
Benjamin Gill <benjamin.gill@crowncommercial.gov.uk>
2022-06-30 11:30:04 +0100

    Add test for stripping phone numbers

--- src/test/java/uk/gov/ccs/conclave/data/migration/service/ContactServiceTest.java
+++ src/test/java/uk/gov/ccs/conclave/data/migration/service/ContactServiceTest.java
@@ -67,4 +67,18 @@ public class ContactServiceTest {
         assertThat(argumentCaptor.getValue().getContactPointName()).isEqualTo("name");
         assertThat(argumentCaptor.getValue().getContacts().size()).isEqualTo(5);
     }
+
+    @Test
+    public void testStripUnwantedCharacters() throws Exception {
+        contactService.migrateUserContact(
+                new User().contactName("name").contactFax("fax- ()").contactPhone("phone()-- ").contactMobile("mobile- (-- )("), "userId",
+                new Org()
+        );
+
+        ArgumentCaptor<ContactRequestInfo> argumentCaptor = ArgumentCaptor.forClass(ContactRequestInfo.class);
+        verify(conclaveClient).createUserContact(eq("userId"), argumentCaptor.capture());
+        assertThat(argumentCaptor.getValue().getContacts().get(0).getContactValue()).isEqualTo("phone");
+        assertThat(argumentCaptor.getValue().getContacts().get(1).getContactValue()).isEqualTo("mobile");
+        assertThat(argumentCaptor.getValue().getContacts().get(2).getContactValue()).isEqualTo("fax");
+    }
 }
