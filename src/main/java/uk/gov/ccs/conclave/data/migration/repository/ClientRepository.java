7536880ba95429c56571c9e83f3b28d5f786c776
@tberey <tomberey1@gmail.com>
2022-07-25 20:59:21 +0100

    TESTING-CON-2082-TESTING

--- src/main/java/uk/gov/ccs/conclave/data/migration/repository/ClientRepository.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/repository/ClientRepository.java
@@ -1,12 +1,12 @@
 package uk.gov.ccs.conclave.data.migration.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.Query;
 
 import uk.gov.ccs.conclave.data.migration.domain.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-    @Query("SELECT * FROM clients WHERE clients.api_key = ?1")
-    ClientRepository findClient(String apiKey);
+    //@Query("SELECT * FROM clients WHERE clients.api_key = ?1")
+    //ClientRepository findClient(String apiKey);
 }
