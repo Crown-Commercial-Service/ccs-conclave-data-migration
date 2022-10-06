128675f622663a1533c4fe64e4240ccd176b7855
@tberey <tomberey1@gmail.com>
2022-10-06 15:45:42 +0100

    CON-3023-Fix-Regression-404-Issues

--- src/main/java/uk/gov/ccs/conclave/data/migration/service/MigrationService.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/service/MigrationService.java
@@ -3,11 +3,9 @@ package uk.gov.ccs.conclave.data.migration.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
 import uk.gov.ccs.conclave.data.migration.domain.Client;
 import uk.gov.ccs.conclave.data.migration.exception.DataMigrationException;
 import uk.gov.ccs.swagger.dataMigration.model.Organisation;
-
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -19,28 +17,30 @@ import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 import static org.springframework.http.HttpStatus.valueOf;
 import static uk.gov.ccs.conclave.data.migration.service.ErrorService.*;
 
+
 @Service
 @RequiredArgsConstructor
 public class MigrationService {
 
     private final OrganisationService organisationService;
-
     private final UserService userService;
-
     private final ReportService reportService;
-
     private final ErrorService errorService;
 
 
     public void migrate(List<Organisation> organisations) {
+
         LocalDateTime startTime = now();
         long failedUserCount = 0;
         long processedUserCount = 0;
         boolean migrationStatus = false;
+
         try {
+
             for (Organisation organisation : organisations) {
                 var orgMigrationResponse = organisationService.migrateOrganisation(organisation);
                 var users = organisation.getUser();
+
                 if (orgMigrationResponse != null && isNotEmpty(users)) {
                     failedUserCount += userService.migrateUsers(users, orgMigrationResponse);
                     processedUserCount += users.size();
@@ -48,6 +48,7 @@ public class MigrationService {
                 }
 
             }
+
             LocalDateTime endTime = now();
             reportService.generateReport(startTime, endTime, organisations, failedUserCount, processedUserCount, migrationStatus ? MIGRATION_STATUS_COMPLETE : MIGRATION_STATUS_PARTIAL);
 
@@ -62,6 +63,7 @@ public class MigrationService {
         if (record.isPresent()) {
             return true;
         }
+
         return false;
     }
 
