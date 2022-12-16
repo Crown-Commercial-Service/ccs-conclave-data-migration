df21cdb99f6aca46d907782b6b948eed9ceb1db3
@tberey <tomberey1@gmail.com>
2022-12-16 13:39:46 +0000

    CON-2411-AWS-SSM-Replace-Vault-Testing

--- src/main/java/uk/gov/ccs/conclave/data/migration/config/VaultMigrationConfig.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/config/VaultMigrationConfig.java
@@ -36,7 +36,9 @@ public class VaultMigrationConfig extends AbstractVaultConfiguration {
         System.out.println("\nHERE-->0001!!\n");
         //System.out.println(cfEnv.findCredentialsByName("conclave-data-migration-integration-ssm-service"));
         //System.out.println(cfEnv.findCredentialsByLabel("user-provided"));
-        System.out.println(cfEnv.findCredentialsByTag("aws-ssm"));
+        //System.out.println(cfEnv.findCredentialsByTag("aws-ssm"));
+        CfCredentials awsCredentials = cfEnv.findCredentialsByTag("aws-ssm");
+        System.out.println(awsCredentials.getString("aws_access_key_id"));
         //System.out.println(cfEnv.findAllServices());
         System.out.println("\nHERE-->0002!!\n");
         return cfEnv.findCredentialsByLabel("hashicorp-vault");
