cf3c7ca75d8299b1931351237889230a4be1df92
@tberey <tomberey1@gmail.com>
2022-12-16 16:52:15 +0000

    CON-2411-AWS-SSM-Replace-Vault-Testing

--- src/main/java/uk/gov/ccs/conclave/data/migration/config/MigrationProperties.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/config/MigrationProperties.java
@@ -7,18 +7,73 @@ import lombok.Setter;
 @Setter
 public class MigrationProperties {
 
-    private String ciiApiKey;
+    private static String ciiApiKey;
 
-    private String ciiDeleteToken;
+    private static String ciiDeleteToken;
 
-    private String ciiOrigin;
+    private static String ciiOrigin;
 
-    private String conclaveApiKey;
+    private static String conclaveApiKey;
 
-    private String conclaveOrigin;
+    private static String conclaveOrigin;
 
-    private boolean sendUserRegistrationEmail;
+    private static boolean sendUserRegistrationEmail;
 
-    private boolean accountVerified;
+    private static boolean accountVerified;
 
+    public static String getCiiApiKey() {
+        return ciiApiKey;
+    }
+
+    public static void setCiiApiKey(String ciiApiKey) {
+        MigrationProperties.ciiApiKey = ciiApiKey;
+    }
+
+    public static String getCiiDeleteToken() {
+        return ciiDeleteToken;
+    }
+
+    public static void setCiiDeleteToken(String ciiDeleteToken) {
+        MigrationProperties.ciiDeleteToken = ciiDeleteToken;
+    }
+
+    public static String getCiiOrigin() {
+        return ciiOrigin;
+    }
+
+    public static void setCiiOrigin(String ciiOrigin) {
+        MigrationProperties.ciiOrigin = ciiOrigin;
+    }
+
+    public static String getConclaveApiKey() {
+        return conclaveApiKey;
+    }
+
+    public static void setConclaveApiKey(String conclaveApiKey) {
+        MigrationProperties.conclaveApiKey = conclaveApiKey;
+    }
+
+    public static String getConclaveOrigin() {
+        return conclaveOrigin;
+    }
+
+    public static void setConclaveOrigin(String conclaveOrigin) {
+        MigrationProperties.conclaveOrigin = conclaveOrigin;
+    }
+
+    public static boolean isSendUserRegistrationEmail() {
+        return sendUserRegistrationEmail;
+    }
+
+    public static void setSendUserRegistrationEmail(boolean sendUserRegistrationEmail) {
+        MigrationProperties.sendUserRegistrationEmail = sendUserRegistrationEmail;
+    }
+
+    public static boolean isAccountVerified() {
+        return accountVerified;
+    }
+
+    public static void setAccountVerified(boolean accountVerified) {
+        MigrationProperties.accountVerified = accountVerified;
+    }
 }
