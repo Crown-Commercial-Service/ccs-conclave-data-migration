b57d35480b6510e9d5f83ad4c1475f7b11553979
Tom <thomas.berey@crowncommercial.gov.uk>
2023-01-16 17:57:47 +0000

    Update MigrationApplication.java

--- src/main/java/uk/gov/ccs/conclave/data/migration/MigrationApplication.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/MigrationApplication.java
@@ -2,11 +2,7 @@ package uk.gov.ccs.conclave.data.migration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("uk.gov.ccs.conclave.data.migration.*")
-@EntityScan("uk.gov.ccs.conclave.data.migration.domain.*")
 @SpringBootApplication
 public class MigrationApplication {
 
