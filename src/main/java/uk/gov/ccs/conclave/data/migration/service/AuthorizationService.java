c39362a618ec53906b4db4453db0681e34f4c7e3
@tberey <tomberey1@gmail.com>
2023-02-08 17:47:32 +0000

    CON-2010-CSV-Migration-Feature

--- src/main/java/uk/gov/ccs/conclave/data/migration/service/AuthorizationService.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/service/AuthorizationService.java
@@ -4,17 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import uk.gov.ccs.conclave.data.migration.domain.Client;
+import static uk.gov.ccs.conclave.data.migration.service.ErrorService.*;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.xml.bind.DatatypeConverter;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthorizationService {
     
     private static final Logger log = LoggerFactory.getLogger(ContactService.class);
+
+    private final ErrorService errorService;
 
     private static String randomAESKeyGen(final int keyLen) throws NoSuchAlgorithmException {
 
@@ -25,15 +32,33 @@ public class AuthorizationService {
         return DatatypeConverter.printHexBinary(encoded);
     }
 
-    public static String createNewApiKey() {
+    public boolean invalidClientApiKey(String key) {
+        // The following null or empty check is not needed, but saves time/resources if we don't need to check the database.
+        if (key == null || key.trim().isEmpty()) {
+            return true;
+        }
+
+        Optional<Client> record = errorService.findApiKey(key);
+        if (record.isPresent()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void createClientApiKey() {
         String key = null;
-            try {
-                key = AuthorizationService.randomAESKeyGen(256);
-                log.info("Successfully generated new key.");
-            } catch (NoSuchAlgorithmException e) {
-                log.error("Exception caught when creating new key.");
-                e.printStackTrace();
-            }
-            return key;
+        try {
+            key = AuthorizationService.randomAESKeyGen(256);
+            log.info("Successfully generated new key.");
+
+        } catch (NoSuchAlgorithmException e) {
+            log.error("Exception caught when creating new key.");
+            e.printStackTrace();
+            return;
+        }
+
+        String description = "CCS Testing Team " + new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        errorService.saveNewApiKey(key, description);
     }
 }
