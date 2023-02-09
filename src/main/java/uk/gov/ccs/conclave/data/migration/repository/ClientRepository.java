ebbc4f1c5b42ce2a478989aa91726476bbe19e09
@tberey <tomberey1@gmail.com>
2023-02-09 15:50:34 +0000

    CON-2010-CSV-Migration-Feature

--- src/main/java/uk/gov/ccs/conclave/data/migration/repository/ClientRepository.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/repository/ClientRepository.java
@@ -3,8 +3,10 @@ package uk.gov.ccs.conclave.data.migration.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import uk.gov.ccs.conclave.data.migration.domain.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
     Optional<Client> findByApiKey(String apiKey);
 }
