22ba271c87c801197349889bf30a0c40bb0ff509
@tberey <tomberey1@gmail.com>
2023-02-16 15:15:19 +0000

    CON-2010-CSV-Migration-Feature

--- src/main/java/uk/gov/ccs/conclave/data/migration/config/ApplicationConfig.java
+++ src/main/java/uk/gov/ccs/conclave/data/migration/config/ApplicationConfig.java
@@ -6,8 +6,8 @@ import uk.gov.ccs.conclave.data.migration.service.AuthorizationService;
 
 @Configuration
 public class ApplicationConfig {
-    // @Bean
-    // public AuthorizationService authorizationService() {
-    //     return new AuthorizationService();
-    // }
+    @Bean
+    public AuthorizationService authorizationService() {
+        return new AuthorizationService();
+    }
 }
