2e1a8312cfba47bb6aa3d795419ba76d2fdeb99a
jhbgov <148862133+jhbgov@users.noreply.github.com>
2024-06-06 08:41:33 +0100

    adding healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post '/data-migration/migrate/format/csv', to: 'data_migration#validate_as_csv'
   post '/data-migration/migrate/format/json', to: 'data_migration#validate_as_json'
+  match '/health_check', to: proc { [200, {}, ['success']] }, via: [:get]
 end
