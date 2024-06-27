10beb50f8b929a00b5122e9c86c6263eb4cfd0ca
tberey <tomberey1@gmail.com>
2024-06-27 16:51:11 +0100

    PPG-1227--Add-all-the-found-roles

--- app/services/migrate/cii.rb
+++ app/services/migrate/cii.rb
@@ -44,7 +44,7 @@ module Migrate
 
       uri = URI.parse(ENV.fetch('CII_DOMAIN', nil) + "/identities/organisations/schemes/#{organisation_id_type}/identifiers/#{organisation_id}")
       http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = false # Set to false, if using HTTP (or locally hosting).
+      http.use_ssl = true # Set to false, if using HTTP (or locally hosting).
 
       request = Net::HTTP::Post.new(uri.request_uri)
       request['x-api-key'] = ENV.fetch('CII_API_KEY', nil)
